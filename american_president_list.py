ce0c4f02af590b5c2b21661bf046663e3a7fd655
Kaung Myat Han <arkarhtethan@gmail.com>
2019-04-25 15:17:18 +0630

    Python web scraping project to get president american list

---
+++ american_president_list.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States"
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+table = soup.find('table',class_="wikitable")
+
+for link in table.find_all('b'):
+    
+    name = link.find('a')
+    
+    print(name.get_text('title'))
+
+print(page.status_code)
